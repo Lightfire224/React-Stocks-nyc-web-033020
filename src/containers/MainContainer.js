505aec4055c9ad2afc0abff8c39a8d21a2a86606
lightfire224 <elias.taveras@columbia.edu>
2020-06-10 15:39:12 -0400

    push

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import StockContainer from './StockContainer'
 import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
-
+console.log(1+1)
 class MainContainer extends Component {
 
   state = {
